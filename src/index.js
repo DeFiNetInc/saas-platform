4a14b391470f53252db30581c19039de3b3c95ac
leo-guinan <leo@guinantech.com>
2021-02-19 12:42:42 -0500

    removing branch logic for oauth endpoints

--- src/index.js
+++ src/index.js
@@ -8,30 +8,6 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
-// Assuming you have two redirect URIs, and the first is for localhost and second is for production
-const [
-  localRedirectSignIn,
-  productionRedirectSignIn,
-] = awsConfig.oauth.redirectSignIn.split(",");
-
-const [
-  localRedirectSignOut,
-  productionRedirectSignOut,
-] = awsConfig.oauth.redirectSignOut.split(",");
-
-const updatedAwsConfig = {
-  ...awsConfig,
-  oauth: {
-    ...awsConfig.oauth,
-    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
-    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
-  }
-}
-
-Amplify.configure(updatedAwsConfig);
-
-
-
 ReactDOM.render(
   <App />,
   document.getElementById("root")
